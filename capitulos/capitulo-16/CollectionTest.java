1d295290d91327980e442956408cd4ec94bb522a
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2019-12-10 12:43:27 -0300

    LinkedList

--- capitulos/capitulo-16/CollectionTest.java
+++ capitulos/capitulo-16/CollectionTest.java
@@ -19,6 +19,29 @@ public class CollectionTest{
 
         for(String color : removeColors)
             removeList.add(color);
+
+        System.out.println("ArrayList: ");
+        
+        for(int count = 0; count < list.size(); count++)
+            System.out.printf("%s ", list.get(count));
+
+        removeColors(list, removeList);
+
+        System.out.printf("%n%nArrayList after calling removeColors:%n");
+
+        for(String color : list)
+            System.out.printf("%s ", color);
+
+        
+    }
+
+    public static void removeColors(Collection<String> cl1, Collection<String> cl2){
+        Iterator<String> iterator = cl1.iterator();
+        while(iterator.hasNext()){
+            if(cl2.contains(iterator.next()))
+                iterator.remove();
+        }
+
     }
 
 }
